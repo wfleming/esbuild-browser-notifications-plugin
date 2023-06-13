df80cd4174a17692677c1a2def3d9f37b688133a
Will Fleming <will@flemi.ng>
2023-06-13 12:09:22 -0400

    track if bundles have actually changed, only notify when they have
    In situations with many entrypoints in a single `esbuild.build`, esbuild
    annoyingly rebuilds *everything* when any one file changes, even if only
    one bundle depends on the change. So any `onEnd` plugins would get what
    is effectively a bunch of noise of finished-but-unchanged outputs.
    
    Because of that, I was doing one-build-per-entrypoint in my app. But I'm
    finding that watch mode performance suffers badly with this approach.
    CPU usage seems to scale roughly with number of builds, so once I ported
    a big chunk of our endpoints watch mode CPU usage climbed to ~50% of a
    CPU when just idling, which is unacceptable.
    
    As a result I'm shifting my app back to many-entrypoints-per-build,
    so now I need to work around the rebuild-everything behavior so this
    plugin doesn't spew a bunch of pointless finished notifications. (This
    still seems like less of a hassle than writing my own watch code.)
    
    I considered reworking this plugin to require explicit calls of what
    changed from an `onEnd` plugin within the actual app config, but I think
    the overhead of calculating & tracking bundle hashes here is pretty
    small. Keeping the plugin more plug-and-play is preferable as long as
    performance is acceptable, so I'm going with this for now.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import {EventEmitter} from "node:events"
 import {readFileSync} from "node:fs"
+import fs from "node:fs/promises"
+import { createHash } from "node:crypto"
 import path from "node:path"
 import ansi2html from "./ansi2html"
 import {Message, transformSync, formatMessages as esbuildFormatMessages} from "esbuild"
@@ -64,10 +66,38 @@ export function browserNotificationsPlugin(options: BrowserNotificationsPluginOp
         build.initialOptions.banner = { js: clientJsStr }
       }
 
+      const seenBundles: Map<string, string> = new Map() // entrypoint -> sha1 of output
+
       build.onEnd(async result => {
         let bundles: string[] = []
         if (result.metafile && result.metafile.outputs) {
           bundles = Object.keys(result.metafile.outputs).filter(p => !p.endsWith(".map"))
+
+
+          // filter to bundles that we either haven't seen or have changed
+          const changedBundles: string[] = []
+          for (let outPath of bundles) {
+            const entryPoint = result.metafile.outputs[outPath].entryPoint
+            const lastSeenHash = seenBundles.get(entryPoint)
+
+            // output paths are relative to this if present, normalize to read it
+            if (build.initialOptions.absWorkingDir) {
+              outPath = path.normalize(path.join(build.initialOptions.absWorkingDir, outPath))
+            }
+            const newHash = createHash("sha1").update(await fs.readFile(outPath)).digest("hex")
+
+            if (lastSeenHash !== newHash) {
+              seenBundles.set(entryPoint, newHash)
+              changedBundles.push(outPath)
+            }
+          }
+          bundles = changedBundles
+
+          // now normalize everything to absWorkingDir if present, not earlier or we couldn't easily
+          // look it up in result.metafile
+          if (build.initialOptions.absWorkingDir) {
+            bundles = bundles.map(p => path.normalize(path.join(build.initialOptions.absWorkingDir, p)))
+          }
 
           // make the names relative to outdir if present
           if (build.initialOptions.outdir) {
